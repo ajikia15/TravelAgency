c0867cb02b8869cf738d1f3e059d14279fc9f708
ajikia15 <ajikia15@gmail.com>
2023-02-22 16:46:28 +0400

    Adding Search and Filter (Style Only)

---
+++ src/components/Search.svelte
@@ -0,0 +1,23 @@
+<div class="grid w-full -translate-y-1/2 place-items-center">
+	<div
+		class="relative grid w-11/12 transform grid-cols-2 flex-row items-center rounded-xl bg-white p-6 text-2xl shadow-md transition duration-500 after:absolute after:left-0 after:-top-[1px] after:h-2 after:w-full after:rounded-t-xl after:bg-emerald-900 hover:scale-105 hover:shadow-lg xl:w-1/2">
+		<div class="flex rounded-lg bg-gray-100 p-4">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="h-6 w-6 opacity-30"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor">
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+			</svg>
+			<input
+				class="w-60 bg-gray-100  outline-none"
+				type="text"
+				placeholder="Article name or keyword..." />
+		</div>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,23 @@
 <script>
 	import Hero from '../components/Hero.svelte';
 	import Card from '../components/Card.svelte';
-	import { tours } from '../utils/tours.js';
-	import { fade } from 'svelte/transition';
+	import Search from '../components/Search.svelte';
+	import { tours } from '../utils/tours.ts';
 </script>
 
 <svelte:head>
 	<title>Travel Agency</title>
 </svelte:head>
 <Hero />
+<Search />
 <section class="mt-12 grid w-full  place-items-center">
 	<div class="mx-4 grid grid-cols-1 gap-10 md:grid-cols-2 xl:grid-cols-3" id="tours">
 		{#each tours as tour}
-			<a href={`/tour/${tour.id}`}>
-				<Card {tour} />
-			</a>
+			{#if tour.availability}
+				<a href={`/tour/${tour.id}`}>
+					<Card {tour} />
+				</a>
+			{/if}
 		{/each}
 	</div>
 </section>
