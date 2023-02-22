c0b860f68f004ffbbff4b6dee7f6252190568d58
ajikia15 <ajikia15@gmail.com>
2023-02-22 18:19:56 +0400

    First working Search // TODO turn into a component

--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -1,6 +1,10 @@
+<script>
+	export let searchQuery = '';
+</script>
+
 <div class="grid w-full -translate-y-1/2 place-items-center">
 	<div
-		class="relative grid w-11/12 transform grid-cols-2 flex-row items-center rounded-xl bg-white p-6 text-2xl shadow-md transition duration-500 after:absolute after:left-0 after:-top-[1px] after:h-2 after:w-full after:rounded-t-xl after:bg-emerald-900 hover:scale-105 hover:shadow-lg xl:w-1/2">
+		class="relative grid w-11/12 transform grid-cols-1 flex-row items-center gap-3 rounded-xl bg-white p-6 text-2xl shadow-md transition duration-500 after:absolute after:left-0 after:-top-[1px] after:h-2 after:w-full  after:rounded-t-xl hover:scale-105 hover:shadow-lg md:grid-cols-2 xl:w-1/2">
 		<div class="flex rounded-lg bg-gray-100 p-4">
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
@@ -17,7 +21,9 @@
 			<input
 				class="w-60 bg-gray-100  outline-none"
 				type="text"
-				placeholder="Article name or keyword..." />
+				placeholder="Where To?"
+				bind:value={searchQuery} />
 		</div>
+		<div>{searchQuery} we are supposed to have a slider here.</div>
 	</div>
 </div>
