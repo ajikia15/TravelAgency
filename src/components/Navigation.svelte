437189323b594a44898705f2a277cc5cb4cf5900
ajikia15 <ajikia15@gmail.com>
2023-02-28 00:03:57 +0400

    MOBILENAV

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -60,9 +60,9 @@
 </nav>
 <!-- Mobile Nav -->
 <nav
-	class="fixed bottom-0 left-0 z-10 grid h-[15%] min-h-max w-full place-items-center bg-black text-white md:hidden">
+	class="fixed bottom-0 left-0 z-10 grid h-[12%] min-h-max w-full place-items-center bg-white text-emerald-800 shadow-2xl md:hidden">
 	<ul class="grid w-11/12 grid-cols-4">
-		<li class="flex flex-col items-center">
+		<li class="relative flex cursor-pointer flex-col items-center">
 			<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"
 				><path
 					fill="none"
@@ -77,23 +77,23 @@
 					stroke-linejoin="round"
 					stroke-width="32"
 					d="M480 256L266.89 52c-5-5.28-16.69-5.34-21.78 0L32 256m368-77V64h-48v69" /></svg>
-			<a href="/" class="">Home</a>
+			<a href="/" class="flex justify-center"><p>Home</p></a>
 		</li>
-		<li class="flex flex-col items-center">
+		<li class="relative flex cursor-pointer flex-col items-center">
 			<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"
 				><path
 					fill="currentColor"
 					d="M5 22V2.975q0-.425.288-.7T6 2q.425 0 .713.288T7 3v1h12.525q.525 0 .825.438t.1.937L19 9l1.45 3.625q.2.5-.1.938t-.825.437H7v8H5ZM7 6v6v-6Zm5.5 5q.825 0 1.413-.588T14.5 9q0-.825-.588-1.413T12.5 7q-.825 0-1.413.588T10.5 9q0 .825.588 1.413T12.5 11ZM7 12h11.05l-1.2-3l1.2-3H7v6Z" /></svg>
 			<a href="/#tours" class="">Tours</a>
 		</li>
-		<li class="flex flex-col items-center">
+		<li class="relative flex cursor-pointer flex-col items-center">
 			<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"
 				><path
 					fill="currentColor"
 					d="M11 17h2v-6h-2v6Zm1-8q.425 0 .713-.288T13 8q0-.425-.288-.713T12 7q-.425 0-.713.288T11 8q0 .425.288.713T12 9Zm0 13q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22Zm0-2q3.35 0 5.675-2.325T20 12q0-3.35-2.325-5.675T12 4Q8.65 4 6.325 6.325T4 12q0 3.35 2.325 5.675T12 20Zm0-8Z" /></svg>
 			<a href="/about" class="">About Us</a>
 		</li>
-		<li class="flex flex-col items-center">
+		<li class="relative flex cursor-pointer flex-col items-center">
 			<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"
 				><path
 					fill="currentColor"
