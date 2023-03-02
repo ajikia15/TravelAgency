4d65f309b35277fb0f89990548b64c7c5cf73f98
ajikia15 <ajikia15@gmail.com>
2023-03-02 22:56:10 +0400

    Cool focus-within Effect

--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -2,7 +2,11 @@
 	export let searchQuery: string = '';
 </script>
 
-<div class="flex items-center border-b border-b-moss-500 bg-white p-2">
+<div
+	class="group relative box-border flex items-center p-2 before:pointer-events-none before:absolute before:top-2.5 before:left-10 before:z-10 before:text-moss-500 before:transition-all before:content-['Where\0020To?'] after:absolute after:-bottom-0.5 after:h-0.5 after:w-full after:bg-moss-500 after:transition-all focus-within:before:-top-3 focus-within:before:text-sm focus-within:after:h-1	 {searchQuery.length >
+	0
+		? 'before:-top-3 before:text-sm'
+		: ''} ">
 	<svg
 		xmlns="http://www.w3.org/2000/svg"
 		class="h-6 w-6 opacity-30"
@@ -16,8 +20,7 @@
 			d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
 	</svg>
 	<input
-		class="flex w-60 bg-white pl-2 text-moss-500 outline-none placeholder:text-moss-500"
+		class="relative flex w-full bg-white pl-2 text-moss-500 outline-none"
 		type="text"
-		placeholder="Where To?"
 		bind:value={searchQuery} />
 </div>
