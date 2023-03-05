b437032982453b005d27906f5160f21e934d09dc
ajikia15 <ajikia15@gmail.com>
2023-03-05 13:16:50 +0400

    Hero Erased

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,18 +1,3 @@
-<section
-	class="relative z-[-1] grid h-[70vh] w-full place-items-center bg-[url('/hero-svaneti-2.jpg')] bg-cover bg-top pt-0 after:absolute after:z-[-1] after:h-full after:w-full after:backdrop-blur-md md:pt-14">
-	<div class="grid h-5/6 w-11/12 grid-cols-2 gap-4 border-red-600 ">
-		<header class="grid border-yellow-400 text-gray-300">
-			<div class="flex flex-col justify-center ">
-				<div class="">
-					<p class="text-6xl">Svaneti</p>
-					<p class="pt-8 text-xl">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, dicta.
-					</p>
-				</div>
-			</div>
-		</header>
-		<aside class="grid place-items-end border-yellow-200">
-			<div class="flex h-full w-5/6 items-center justify-center rounded-md  text-white" />
-		</aside>
-	</div>
+<section class="grid h-[70vh] bg-white dark:bg-gray-700 dark:text-white">
+	<h1>hero</h1>
 </section>
