bc6d57b12a8f1203626a48b772cf4b5429009a6b
ajikia15 <ajikia15@gmail.com>
2023-03-10 14:35:46 +0400

    wip

--- src/components/VehicleInformation.svelte
+++ src/components/VehicleInformation.svelte
@@ -1,22 +1,46 @@
-<article class="grid w-full place-items-center bg-white">
+<article class="grid w-full place-items-center bg-white py-10">
 	<div class="w-11/12">
 		<div class="flex flex-col items-center justify-center">
 			<h1>What does the team offer?</h1>
 			<h2>List Of Vehicles:</h2>
 		</div>
-		<div class="grid grid-cols-3">
-			<div class="flex justify-center">
-				<h3>Car</h3>
-				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+		<div class="grid grid-cols-3 ">
+			<div class="flex flex-col items-center justify-center">
+				<h3>Sedan</h3>
+				<svg
+					class="text-gray-300"
+					xmlns="http://www.w3.org/2000/svg"
+					width="128"
+					height="128"
+					viewBox="0 0 24 24"
+					><path
+						fill="currentColor"
+						d="M16 6H6l-5 6v3h2a3 3 0 0 0 3 3a3 3 0 0 0 3-3h6a3 3 0 0 0 3 3a3 3 0 0 0 3-3h2v-3c0-1.11-.89-2-2-2h-2l-3-4M6.5 7.5h4V10h-6l2-2.5m5.5 0h3.5l1.96 2.5H12V7.5m-6 6A1.5 1.5 0 0 1 7.5 15A1.5 1.5 0 0 1 6 16.5A1.5 1.5 0 0 1 4.5 15A1.5 1.5 0 0 1 6 13.5m12 0a1.5 1.5 0 0 1 1.5 1.5a1.5 1.5 0 0 1-1.5 1.5a1.5 1.5 0 0 1-1.5-1.5a1.5 1.5 0 0 1 1.5-1.5Z" /></svg>
+			</div>
+			<div class="flex flex-col items-center justify-center">
+				<h3>Jeep</h3>
+				<svg
+					class="text-gray-300"
+					xmlns="http://www.w3.org/2000/svg"
+					width="128"
+					height="128"
+					viewBox="0 0 24 24"
 					><path
 						fill="currentColor"
 						d="M3 6h13l3 4h2c1.11 0 2 .89 2 2v3h-2a3 3 0 0 1-3 3a3 3 0 0 1-3-3H9a3 3 0 0 1-3 3a3 3 0 0 1-3-3H1V8c0-1.11.89-2 2-2m-.5 1.5V10h8V7.5h-8m9.5 0V10h5.14l-1.89-2.5H12m-6 6A1.5 1.5 0 0 0 4.5 15A1.5 1.5 0 0 0 6 16.5A1.5 1.5 0 0 0 7.5 15A1.5 1.5 0 0 0 6 13.5m12 0a1.5 1.5 0 0 0-1.5 1.5a1.5 1.5 0 0 0 1.5 1.5a1.5 1.5 0 0 0 1.5-1.5a1.5 1.5 0 0 0-1.5-1.5Z" /></svg>
 			</div>
-			<div class="flex justify-center">
-				<h3>Car</h3>
-			</div>
-			<div class="flex justify-center">
-				<h3>Car</h3>
+
+			<div class="flex flex-col items-center justify-center">
+				<h3>Tank</h3>
+				<svg
+					class="text-gray-300"
+					xmlns="http://www.w3.org/2000/svg"
+					width="128"
+					height="128"
+					viewBox="0 0 512 512"
+					><path
+						fill="currentColor"
+						d="m240.287 127.394l-23.78-19.145l108.861-13.892l2.751 21.828zM180.746 107.7h-62.91l-17.88 44.576h136.067zm275.78 247.033a62.91 62.91 0 1 0-62.911 62.91a62.91 62.91 0 0 0 62.91-62.91zm-279.205 0a62.91 62.91 0 1 0-62.91 62.91a62.91 62.91 0 0 0 62.91-62.91zM486 347.25a20.548 20.548 0 0 1-7.922 16.216a84.917 84.917 0 1 0-168.362 4.388H198.31a84.903 84.903 0 1 0-168.788-13.686a26.834 26.834 0 0 1-3.329-16.504l16.753-143.165a22.914 22.914 0 0 1 22.762-20.26h241.904a14.992 14.992 0 0 1 11.787 5.736l44.012 56.089l105.383 22.941zM337.815 239.255l-35.856-50.656h-93.265v50.683h129.135z" /></svg>
 			</div>
 		</div>
 	</div>
