4d6d47013a28eb0539358a49b0cd0e40b41ac7f6
ajikia15 <ajikia15@gmail.com>
2023-03-24 01:04:34 +0400

    Post Page 75%

--- src/routes/tour/[id]/+page.svelte
+++ src/routes/tour/[id]/+page.svelte
@@ -219,19 +219,31 @@
 						>Book</button>
 				</div>
 				<div class="grid w-full grid-cols-3 gap-2 rounded text-moss-500">
-					<div class="grid place-items-center rounded border bg-white p-2 shadow">
-						<svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24"
+					<div
+						class="group grid place-items-center rounded border bg-white p-2 shadow transition-all hover:bg-moss-500 hover:text-white">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="28"
+							height="28"
+							viewBox="0 0 24 24"
+							class=""
 							><path
 								fill="currentColor"
 								d="M18 22q-1.25 0-2.125-.875T15 19q0-.175.025-.363t.075-.337l-7.05-4.1q-.425.375-.95.588T6 15q-1.25 0-2.125-.875T3 12q0-1.25.875-2.125T6 9q.575 0 1.1.213t.95.587l7.05-4.1q-.05-.15-.075-.337T15 5q0-1.25.875-2.125T18 2q1.25 0 2.125.875T21 5q0 1.25-.875 2.125T18 8q-.575 0-1.1-.212t-.95-.588L8.9 11.3q.05.15.075.338T9 12q0 .175-.025.363T8.9 12.7l7.05 4.1q.425-.375.95-.587T18 16q1.25 0 2.125.875T21 19q0 1.25-.875 2.125T18 22Zm0-16q.425 0 .713-.288T19 5q0-.425-.288-.713T18 4q-.425 0-.713.288T17 5q0 .425.288.713T18 6ZM6 13q.425 0 .713-.288T7 12q0-.425-.288-.713T6 11q-.425 0-.713.288T5 12q0 .425.288.713T6 13Zm12 7q.425 0 .713-.288T19 19q0-.425-.288-.713T18 18q-.425 0-.713.288T17 19q0 .425.288.713T18 20Zm0-15ZM6 12Zm12 7Z" /></svg>
 					</div>
 					<a
 						href="tel:+995598857374"
-						class="grid place-items-center rounded border bg-white p-2 shadow">
+						class="group grid place-items-center rounded border bg-white p-2 shadow transition-all hover:bg-moss-500 hover:text-white">
 						<p class="text-xl" aria-label="Contact Phone Number">+995 598 85 73 74</p>
 					</a>
-					<div class="flex items-center justify-center gap-1 rounded border bg-white p-2 shadow ">
-						<svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24"
+					<div
+						class="group flex items-center justify-center gap-1 rounded border bg-white p-2 shadow transition-all hover:bg-moss-500 hover:text-white">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class=""
+							width="28"
+							height="28"
+							viewBox="0 0 24 24"
 							><path
 								fill="currentColor"
 								d="m14.777 14.038l2.65-3.92c.262-.386-.235-.805-.615-.529l-2.858 2.015a.571.571 0 0 1-.652 0l-2.116-1.477c-.633-.437-1.538-.277-1.963.335l-2.65 3.92c-.262.386.235.806.615.529l2.858-2.015a.571.571 0 0 1 .652 0l2.116 1.452c.633.462 1.538.302 1.963-.31Z" /><path
@@ -240,7 +252,12 @@
 								d="M12 2.25A9.75 9.75 0 0 0 2.25 12a9.724 9.724 0 0 0 3 7.036V21.5a.75.75 0 0 0 .987.712l2.78-.927A9.745 9.745 0 0 0 12 21.75c5.385 0 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM3.75 12a8.25 8.25 0 1 1 5.516 7.787a.75.75 0 0 0-.486-.004l-2.03.676v-1.75a.75.75 0 0 0-.25-.56A8.228 8.228 0 0 1 3.75 12Z"
 								clip-rule="evenodd" /></svg>
 						|
-						<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class=""
+							width="32"
+							height="32"
+							viewBox="0 0 24 24"
 							><path fill="currentColor" d="M16 7a1 1 0 1 1 2 0a1 1 0 0 1-2 0Z" /><path
 								fill="currentColor"
 								fill-rule="evenodd"
