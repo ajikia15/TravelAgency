feeb7e7897c75ffcc1029a08dc6acff6c77f3663
ajikia15 <aleksandrej15@gmail.com>
2023-06-14 17:27:43 +0400

    Skeleton loading for indiviadual listings

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 	import { onMount } from 'svelte';
 	import Card from '../components/Card.svelte';
 
-	let carousel: any;
+	const iteration = 4;
 	let searchQuery = '';
 	let curMin = 0;
 	let curMax = 1000;
@@ -24,45 +24,26 @@
 		});
 
 		tours = fbTours;
-		console.table(tours);
 	});
+	$: processedSearch = searchQuery.toLowerCase();
+	$: minMax = [curMin, curMax];
+	$: sortedTours = (() => {
+		if (sortOption === 1) {
+			return tours.slice().sort((a, b) => a.Price - b.Price);
+		} else if (sortOption === 2) {
+			return tours.slice().sort((a, b) => b.Price - a.Price);
+		} else if (sortOption === 3) {
+			return tours.slice().sort((a, b) => a.MinPeople - b.MinPeople);
+		} else if (sortOption === 4) {
+			return tours.slice().sort((a, b) => b.MinPeople - a.MinPeople);
+		}
+	})();
 </script>
 
 <svelte:head>
 	<title>Travel Agency</title>
 </svelte:head>
 <Hero />
-
-<section class="mb-5 grid min-h-[100px] w-full place-items-center">
-	<ul class="grid w-11/12 grid-cols-1 gap-10 md:grid-cols-3 xl:grid-cols-4" id="tours">
-		{#if tours.length > 0}
-			{#each tours as tour, i}
-				<Card Location={tour.Location} Price={tour.Price} Pics={tour.Pics} id={tour.id} />
-			{/each}
-		{:else}
-			<div
-				role="status"
-				class="max-w-sm animate-pulse rounded border border-gray-200 p-4 shadow dark:border-gray-700 md:p-6">
-				<div
-					class="mb-4 flex h-48 items-center justify-center rounded bg-gray-300 dark:bg-gray-700">
-					<svg
-						class="h-12 w-12 text-gray-200 dark:text-gray-600"
-						xmlns="http://www.w3.org/2000/svg"
-						aria-hidden="true"
-						fill="currentColor"
-						viewBox="0 0 640 512"
-						><path
-							d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z" /></svg>
-				</div>
-				<div class="mb-4 h-2.5 w-48 rounded-full bg-gray-200 dark:bg-gray-700" />
-				<div class="mb-2.5 h-2 rounded-full bg-gray-200 dark:bg-gray-700" />
-				<div class="mb-2.5 h-2 rounded-full bg-gray-200 dark:bg-gray-700" />
-				<div class="h-2 rounded-full bg-gray-200 dark:bg-gray-700" />
-				<span class="sr-only">Loading...</span>
-			</div>
-		{/if}
-	</ul>
-</section>
 <div class="grid w-full place-items-center pb-4">
 	<div
 		class="mt-10 grid w-11/12 transform grid-cols-1 flex-row items-center gap-3 rounded-xl bg-white p-5 text-2xl shadow-md transition duration-500 after:absolute after:-top-[1px] after:left-0 after:h-2 after:w-full after:rounded-t-xl hover:scale-105 hover:shadow-lg dark:bg-gray-900 md:mt-0 md:grid-cols-[1fr_1fr] lg:w-3/5">
@@ -75,6 +56,50 @@
 		</div>
 	</div>
 </div>
+<section class="mb-5 grid min-h-[100px] w-full place-items-center">
+	<ul class="grid w-11/12 grid-cols-1 gap-10 md:grid-cols-3 xl:grid-cols-4" id="tours">
+		{#if tours.length > 0}
+			{#if sortOption == 0}
+				{#each tours as tour}
+					{#if tour.Location.toLowerCase().includes(processedSearch) && tour.Price > minMax[0] && tour.Price < minMax[1]}
+						<div>
+							<Card Location={tour.Location} Price={tour.Price} Pics={tour.Pics} id={tour.id} />
+						</div>
+					{/if}
+				{/each}
+			{:else}
+				{#each sortedTours as tour}
+					<div>
+						<Card Location={tour.Location} Price={tour.Price} Pics={tour.Pics} id={tour.id} />
+					</div>
+				{/each}
+			{/if}
+		{:else}
+			{#each Array(iteration) as _, i}
+				<div
+					role="status"
+					class="max-w-sm animate-pulse rounded border border-gray-200 p-4 shadow dark:border-gray-700 md:p-6">
+					<div
+						class="mb-4 flex h-48 items-center justify-center rounded bg-gray-300 dark:bg-gray-700">
+						<svg
+							class="h-12 w-12 text-gray-200 dark:text-gray-600"
+							xmlns="http://www.w3.org/2000/svg"
+							aria-hidden="true"
+							fill="currentColor"
+							viewBox="0 0 640 512"
+							><path
+								d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z" /></svg>
+					</div>
+					<div class="mb-4 h-2.5 w-48 rounded-full bg-gray-200 dark:bg-gray-700" />
+					<div class="mb-2.5 h-2 rounded-full bg-gray-200 dark:bg-gray-700" />
+					<div class="mb-2.5 h-2 rounded-full bg-gray-200 dark:bg-gray-700" />
+					<div class="h-2 rounded-full bg-gray-200 dark:bg-gray-700" />
+					<span class="sr-only">Loading...</span>
+				</div>
+			{/each}
+		{/if}
+	</ul>
+</section>
 
 <style lang="scss">
 </style>
--- src/routes/tour/[slug]/+page.svelte
+++ src/routes/tour/[slug]/+page.svelte
@@ -18,75 +18,98 @@
 	function getImageUrl(number) {
 		return tour.Pics[number];
 	}
-	let galleryState = false;
-	function toggleGalleryState() {
-		galleryState = !galleryState;
-	}
 </script>
 
 <svelte:head>
 	<title>Explore {tour.Location || ''}</title>
 </svelte:head>
 <div class="grid w-full place-items-center py-4 md:pt-[4.5rem]">
-	{#if !galleryState}
-		<div class="grid w-11/12 gap-8 md:grid-cols-[7fr_4fr]">
-			{#if !tour.Location}
-				<div>Loading...</div>
-			{:else}
-				<div class="relative mt-6 flex flex-col overflow-hidden rounded-2xl">
-					<div class="relative grid aspect-video w-full grid-cols-2 overflow-hidden rounded-2xl">
-						<a
-							href="/tour/{slug}/gallery"
-							class="absolute bottom-3 right-3 z-10 cursor-pointer rounded-lg bg-white p-2 px-4">
-							<p>All Photos</p>
-						</a>
-						<div class="object-fit mr-2 h-full rounded-l-2xl bg-zinc-800">
-							<img
-								class="h-full rounded-l-2xl object-cover"
-								src={tour.Pics[0]}
-								alt="Breathtaking view of {tour.Location}" />
-						</div>
-						<div class="grid h-full grid-cols-2 gap-x-2 gap-y-2">
-							<img
-								class="h-full bg-zinc-800 object-cover"
-								src={tour.Pics[1]}
-								alt="Breathtaking view of {tour.Location}" />
-							<img
-								class="h-full bg-zinc-800 object-cover"
-								src={tour.Pics[2]}
-								alt="Breathtaking view of {tour.Location}" />
-							<img
-								class="h-full bg-zinc-800 object-cover"
-								src={tour.Pics[3]}
-								alt="Breathtaking view of {tour.Location}" />
-							<img
-								class="h-full bg-zinc-800 object-cover"
-								src={tour.Pics[4]}
-								alt="Breathtaking view of {tour.Location}" />
-						</div>
+	<div class="grid w-11/12 gap-8 md:grid-cols-[7fr_4fr]">
+		{#if !tour.Location}
+			<div role="status" class="mx-auto w-full animate-pulse">
+				<div class="flex w-full flex-col justify-start">
+					<div class="mb-2 h-2.5 w-40 rounded-full bg-gray-200 dark:bg-gray-700" />
+					<div class="mb-4 h-2.5 w-48 rounded-full bg-gray-200 dark:bg-gray-700" />
+					<div
+						class="mb-2 flex aspect-video w-full items-center justify-center rounded bg-gray-300 dark:bg-gray-700">
+						<svg
+							class="h-12 w-12 text-gray-200"
+							xmlns="http://www.w3.org/2000/svg"
+							aria-hidden="true"
+							fill="currentColor"
+							viewBox="0 0 640 512">
+							<path
+								d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z" />
+						</svg>
 					</div>
-					<div class="space-y-2 py-6 dark:text-white">
-						<h4 class="roboto text-lg font-bold text-gray-400">Why {tour.Location}?</h4>
-						<div class="roboto text-xl">
-							{#each tour.Description.split(/\/(\d+)\//) as segment, index}
-								{#if index % 2 === 0}
-									<span>{segment}</span> <!-- Regular text segment -->
-								{:else}
-									{#each [parseInt(segment, 10)] as imageNumber}
-										<img src={getImageUrl(imageNumber)} alt={'Image ' + imageNumber} />
-										<!-- Image placeholder -->
-									{/each}
-								{/if}
-							{/each}
-						</div>
+					<div class="w-full">
+						<div class="mb-4 h-2.5 w-48 rounded-full bg-gray-200 dark:bg-gray-700" />
+						<div class="mb-2.5 h-2 max-w-[480px] rounded-full bg-gray-200 dark:bg-gray-700" />
+						<div class="mb-2.5 h-2 rounded-full bg-gray-200 dark:bg-gray-700" />
+						<div class="mb-2.5 h-2 max-w-[440px] rounded-full bg-gray-200 dark:bg-gray-700" />
+						<div class="mb-2.5 h-2 max-w-[460px] rounded-full bg-gray-200 dark:bg-gray-700" />
+						<div class="h-2 max-w-[360px] rounded-full bg-gray-200 dark:bg-gray-700" />
+					</div>
+
+					<span class="sr-only">Loading...</span>
+				</div>
+			</div>
+		{:else}
+			<div class="relative mt-6 flex w-full flex-col overflow-hidden rounded-2xl">
+				<div class="relative grid aspect-video w-full grid-cols-2 overflow-hidden rounded-2xl">
+					<a
+						href="/tour/{slug}/gallery"
+						class="absolute bottom-3 right-3 z-10 cursor-pointer rounded-lg bg-white p-2 px-4">
+						<p>All Photos</p>
+					</a>
+					<div class="object-fit mr-2 h-full rounded-l-2xl bg-zinc-800">
+						<img
+							class="h-full rounded-l-2xl object-cover"
+							src={tour.Pics[0]}
+							alt="Breathtaking view of {tour.Location}" />
+					</div>
+					<div class="grid h-full grid-cols-2 gap-x-2 gap-y-2">
+						<img
+							class="h-full bg-zinc-800 object-cover"
+							src={tour.Pics[1]}
+							alt="Breathtaking view of {tour.Location}" />
+						<img
+							class="h-full bg-zinc-800 object-cover"
+							src={tour.Pics[2]}
+							alt="Breathtaking view of {tour.Location}" />
+						<img
+							class="h-full bg-zinc-800 object-cover"
+							src={tour.Pics[3]}
+							alt="Breathtaking view of {tour.Location}" />
+						<img
+							class="h-full bg-zinc-800 object-cover"
+							src={tour.Pics[4]}
+							alt="Breathtaking view of {tour.Location}" />
 					</div>
 				</div>
-			{/if}
+				<div class="space-y-2 py-6 dark:text-white">
+					<h4 class="roboto text-lg font-bold text-gray-400">Why {tour.Location}?</h4>
+					<div class="roboto text-xl">
+						{#each tour.Description.split(/\/(\d+)\//) as segment, index}
+							{#if index % 2 === 0}
+								<span>{segment}</span> <!-- Regular text segment -->
+							{:else}
+								{#each [parseInt(segment, 10)] as imageNumber}
+									<img src={getImageUrl(imageNumber)} alt={'Image ' + imageNumber} />
+									<!-- Image placeholder -->
+								{/each}
+							{/if}
+						{/each}
+					</div>
+				</div>
+			</div>
+		{/if}
 
-			<div class="relative flex flex-col space-y-2">
-				<div class="top-24 flex flex-col space-y-2 md:sticky">
+		<div class="relative flex flex-col space-y-2">
+			<div class="top-24 flex flex-col space-y-2 md:sticky">
+				{#if tour.Location}
 					<div
-						class="flex flex-col justify-center space-y-5 rounded border bg-white p-7 text-center shadow will-change-transform">
+						class="flex w-full flex-col justify-center space-y-5 rounded border bg-white p-7 text-center shadow will-change-transform">
 						<div class="space-y-4">
 							<h1 class="text-4xl">{tour.Location}, Georgia</h1>
 							<h3 class="roboto text-sm text-gray-400">Vacation Package</h3>
@@ -103,7 +126,7 @@
 									><path
 										fill="currentColor"
 										d="M12 4a4 4 0 0 1 4 4a4 4 0 0 1-4 4a4 4 0 0 1-4-4a4 4 0 0 1 4-4m0 10c4.42 0 8 1.79 8 4v2H4v-2c0-2.21 3.58-4 8-4Z" /></svg>
-								For minPeople People
+								For {tour.MinPeople}+ People
 							</li>
 							<li class="flex flex-row items-center gap-2 border-t p-2">
 								<svg
@@ -225,10 +248,51 @@
 									clip-rule="evenodd" /></svg>
 						</div>
 					</div>
-				</div>
+				{:else}
+					<div
+						role="status"
+						class="max-w-full animate-pulse space-y-4 divide-y divide-gray-200 rounded border border-gray-200 p-4 shadow dark:divide-gray-700 dark:border-gray-700 md:p-6">
+						<div class="flex items-center justify-between">
+							<div>
+								<div class="mb-2.5 h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-600" />
+								<div class="h-2 w-32 rounded-full bg-gray-200 dark:bg-gray-700" />
+							</div>
+							<div class="h-2.5 w-12 rounded-full bg-gray-300 dark:bg-gray-700" />
+						</div>
+						<div class="flex items-center justify-between pt-4">
+							<div>
+								<div class="mb-2.5 h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-600" />
+								<div class="h-2 w-32 rounded-full bg-gray-200 dark:bg-gray-700" />
+							</div>
+							<div class="h-2.5 w-12 rounded-full bg-gray-300 dark:bg-gray-700" />
+						</div>
+						<div class="flex items-center justify-between pt-4">
+							<div>
+								<div class="mb-2.5 h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-600" />
+								<div class="h-2 w-32 rounded-full bg-gray-200 dark:bg-gray-700" />
+							</div>
+							<div class="h-2.5 w-12 rounded-full bg-gray-300 dark:bg-gray-700" />
+						</div>
+						<div class="flex items-center justify-between pt-4">
+							<div>
+								<div class="mb-2.5 h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-600" />
+								<div class="h-2 w-32 rounded-full bg-gray-200 dark:bg-gray-700" />
+							</div>
+							<div class="h-2.5 w-12 rounded-full bg-gray-300 dark:bg-gray-700" />
+						</div>
+						<div class="flex items-center justify-between pt-4">
+							<div>
+								<div class="mb-2.5 h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-600" />
+								<div class="h-2 w-32 rounded-full bg-gray-200 dark:bg-gray-700" />
+							</div>
+							<div class="h-2.5 w-12 rounded-full bg-gray-300 dark:bg-gray-700" />
+						</div>
+						<span class="sr-only">Loading...</span>
+					</div>
+				{/if}
 			</div>
 		</div>
-	{/if}
+	</div>
 </div>
 
 <style lang="scss">
