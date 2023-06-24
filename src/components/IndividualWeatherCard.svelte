e58545711ec32a2a3a5f15767eed07a58826168c
ajikia15 <ajikia15@gmail.com>
2023-06-24 16:45:08 +0400

    weather barebones card

---
+++ src/components/IndividualWeatherCard.svelte
@@ -0,0 +1,10 @@
+<script>
+export let temperature;
+export let condition;
+</script>
+
+<div class="flex flex-col items-center">
+	// <img src={`${condition}.svg`} alt="weather condition"/>
+	<svg class="h-10 w-10 fill-current text-gray-400 mt-3" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M6.76 4.84l-1.8-1.79-1.41 1.41 1.79 1.79zM1 10.5h3v2H1zM11 .55h2V3.5h-2zm8.04 2.495l1.408 1.407-1.79 1.79-1.407-1.408zm-1.8 15.115l1.79 1.8 1.41-1.41-1.8-1.79zM20 10.5h3v2h-3zm-8-5c-3.31 0-6 2.69-6 6s2.69 6 6 6 6-2.69 6-6-2.69-6-6-6zm0 10c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4zm-1 4h2v2.95h-2zm-7.45-.96l1.41 1.41 1.79-1.8-1.41-1.41z"/></svg><span class="font-semibold mt-1 text-sm<span class="text-xs font-semibold text-gray-400">PM</span>
+	<span class="font-semibold text-lg">{temperature}°C</span>
+</div>
