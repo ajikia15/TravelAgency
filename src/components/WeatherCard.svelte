ab003398b50c384111c469fe4a92b4a3b9d67b3c
ajikia15 <ajikia15@gmail.com>
2023-07-03 18:15:21 +0400

    weather refinements + mobile individual tour

--- src/components/WeatherCard.svelte
+++ src/components/WeatherCard.svelte
@@ -13,6 +13,8 @@
 		DialogTrigger
 	} from '$components/ui/dialog';
 
+	const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
 	// fetch data using api
 	// display forecast for the same day
 	// display forecast for next few days
@@ -26,74 +28,95 @@
 			}`
 		);
 		data = await res.json();
+		data.daily.forEach((day) => {
+			const date = new Date(day.dt * 1000);
+			day.dayOfWeek = days[date.getDay()];
+			day.dateString = date.toLocaleDateString(undefined, {
+				month: 'long',
+				day: 'numeric'
+			});
+		});
 		// console.log(data);
 	};
 	getWeather();
 </script>
 
 <div class="flex flex-col items-center justify-center text-gray-700">
-	<div class="w-full p-10 bg-white rounded-lg shadow-md">
+	<div class="w-full rounded-lg bg-white px-10 pt-6 shadow-md">
 		<!-- <IndividualWeatherCard {Location} /> -->
 		{#if data.lat}
 			<div class="flex justify-between">
 				<div class="flex flex-col">
 					<span class="text-5xl font-bold">{Math.round(data.daily[0].temp.day)}°C</span>
 					<span class="mt-1 font-semibold text-gray-500">
-						{Location}
+						{Location} Today, {data.daily[0].dateString}
 					</span>
 				</div>
 				{#if data.daily[0].weather[0].main === 'Rain'}
-					<svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24"
+					<svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 24 24"
 						><path
 							fill="currentColor"
 							d="M11 2a6 6 0 0 0-5.986 6.41a5 5 0 0 0-1.322 8.34a1 1 0 1 0 1.324-1.5a3.002 3.002 0 0 1 1.324-5.178a1 1 0 0 0 .757-1.193A4 4 0 1 1 14.92 7.2a1 1 0 0 0 .999.8H16a4 4 0 0 1 2.4 7.2a1 1 0 0 0 1.201 1.6a6 6 0 0 0-2.93-10.762A6.002 6.002 0 0 0 11 2zm1.949 13.316a1 1 0 0 0-1.898-.632l-2 6a1 1 0 0 0 1.898.632l2-6zm3.367-2.265a1 1 0 0 1 .633 1.265l-2 6a1 1 0 0 1-1.898-.632l2-6a1 1 0 0 1 1.265-.633zM9.45 14.316a1 1 0 0 0-1.898-.632l-2 6a1 1 0 0 0 1.898.632l2-6z" /></svg>
 				{/if}
 				{#if data.daily[0].weather[0].main === 'Clouds'}
-					<svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24"
+					<svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 24 24"
 						><path
 							fill="currentColor"
 							d="M16 5a3 3 0 0 1 2.557 4.57a5.96 5.96 0 0 0-1.886-.533a6.019 6.019 0 0 0-2.697-3.25A2.99 2.99 0 0 1 16 5zm-4.055.074a6 6 0 0 0-6.931 6.336A5 5 0 0 0 7 21h9a6 6 0 0 0 4.2-10.285a5 5 0 0 0-8.255-5.64zM7 11a4 4 0 0 1 7.92-.8a1 1 0 0 0 1 .8H16a4 4 0 0 1 0 8H7a3 3 0 0 1-.66-5.927a1 1 0 0 0 .757-1.194A4.017 4.017 0 0 1 7 11z" /></svg>
 				{/if}
 				{#if data.daily[0].weather[0].main === 'Clear'}
-					<svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24"
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="64"
+						height="64"
+						class="text-yellow-400"
+						viewBox="0 0 24 24"
 						><path
 							fill="currentColor"
 							d="M12 2a1 1 0 0 1 1 1v1a1 1 0 1 1-2 0V3a1 1 0 0 1 1-1zm7.071 2.929a1 1 0 0 1 0 1.414l-.707.707a1 1 0 1 1-1.414-1.414l.707-.707a1 1 0 0 1 1.414 0zm-14.142 0a1 1 0 0 1 1.414 0l.707.707A1 1 0 0 1 5.636 7.05l-.707-.707a1 1 0 0 1 0-1.414zM12 8a4 4 0 1 0 0 8a4 4 0 0 0 0-8zm-6 4a6 6 0 1 1 12 0a6 6 0 0 1-12 0zm-4 0a1 1 0 0 1 1-1h1a1 1 0 1 1 0 2H3a1 1 0 0 1-1-1zm17 0a1 1 0 0 1 1-1h1a1 1 0 1 1 0 2h-1a1 1 0 0 1-1-1zM5.636 16.95a1 1 0 0 1 1.414 1.414l-.707.707a1 1 0 0 1-1.414-1.414l.707-.707zm11.314 1.414a1 1 0 0 1 1.414-1.414l.707.707a1 1 0 0 1-1.414 1.414l-.707-.707zM12 19a1 1 0 0 1 1 1v1a1 1 0 1 1-2 0v-1a1 1 0 0 1 1-1z" /></svg>
 				{/if}
 				{#if data.daily[0].weather[0].main === 'Thunderstorm'}
-					<svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24"
+					<svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 24 24"
 						><path
 							fill="currentColor"
 							d="M11 2a6 6 0 0 0-5.986 6.41a5 5 0 0 0 .737 9.432a1 1 0 1 0 .498-1.936a3.002 3.002 0 0 1 .09-5.833a1 1 0 0 0 .758-1.194A4 4 0 1 1 14.92 7.2a1 1 0 0 0 .999.8H16a4 4 0 0 1 1.6 7.668a1 1 0 1 0 .8 1.832a6.001 6.001 0 0 0-1.729-11.463A6.002 6.002 0 0 0 11 2zm1.894 11.447a1 1 0 1 0-1.788-.894l-2 4A1 1 0 0 0 10 18h2.382l-1.276 2.553a1 1 0 1 0 1.788.894l2-4A1 1 0 0 0 14 16h-2.382l1.276-2.553z" /></svg>
 				{/if}
 				{#if data.daily[0].weather[0].main === 'Snow'}
-					<svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24"
+					<svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 24 24"
 						><path
 							fill="currentColor"
 							d="M11 2a6 6 0 0 0-5.986 6.41a5 5 0 0 0-1.322 8.34a1 1 0 1 0 1.324-1.5a3.002 3.002 0 0 1 1.324-5.178a1 1 0 0 0 .757-1.193A4 4 0 1 1 14.92 7.2a1 1 0 0 0 .999.8H16a4 4 0 0 1 2.4 7.2a1 1 0 0 0 1.201 1.6a6 6 0 0 0-2.93-10.762A6.002 6.002 0 0 0 11 2zm3.5 15a1.5 1.5 0 1 0 0-3a1.5 1.5 0 0 0 0 3zm-3.5-.5a1.5 1.5 0 1 1-3 0a1.5 1.5 0 0 1 3 0zm4 3a1.5 1.5 0 1 1-3 0a1.5 1.5 0 0 1 3 0zm-5 1a1.5 1.5 0 1 1-3 0a1.5 1.5 0 0 1 3 0z" /></svg>
 				{/if}
 			</div>
-			<div class="flex mt-3 justify-evenly">
+			<div class="mx-2 mt-3 flex justify-between">
 				{#each data.daily as day, i}
 					{#if i > 0 && i < 4}
-						<IndividualWeatherCard temp={Math.round(day.temp.day)} cond={day.weather[0].main} {i} />
+						<IndividualWeatherCard
+							temp={Math.round(day.temp.day)}
+							cond={day.weather[0].main}
+							{i}
+							date={day.dateString}
+							day={day.dayOfWeek} />
 					{/if}
 				{/each}
 			</div>
 			<Dialog>
-				<DialogTrigger
-					><button type="button" class="line-clamp-1 hover:underline">8 Day Forecast &rarr;</button
-					></DialogTrigger>
+				<DialogTrigger>
+					<button type="button" class="text-muted-foreground line-clamp-1 pb-5 pt-2 hover:underline"
+						>8 Day Forecast &rarr;</button>
+				</DialogTrigger>
 				<DialogContent>
 					<DialogHeader>
 						<DialogTitle>8 day forecast for {Location}</DialogTitle>
 						<DialogDescription>
-							<div class="flex justify-between mt-3">
+							<div class="mt-3 flex justify-between">
 								{#each data.daily as day, i}
 									<IndividualWeatherCard
 										temp={Math.round(day.temp.day)}
 										cond={day.weather[0].main}
-										{i} />
+										{i}
+										date={day.dateString}
+										day={day.dayOfWeek} />
 								{/each}
 							</div>
 						</DialogDescription>
--- src/routes/tour/[slug]/+page.svelte
+++ src/routes/tour/[slug]/+page.svelte
@@ -10,6 +10,16 @@
 		AccordionItem,
 		AccordionTrigger
 	} from '$components/ui/accordion';
+	import { Button } from '$components/ui/button';
+
+	import {
+		Sheet,
+		SheetContent,
+		SheetDescription,
+		SheetHeader,
+		SheetTitle,
+		SheetTrigger
+	} from '$components/ui/sheet';
 	import WeatherCard from '../../../components/WeatherCard.svelte';
 	const slug = $page.params.slug;
 	let tour = {};
@@ -120,8 +130,228 @@
 				</div>
 			</div>
 		{/if}
+		<div class="md:hidden">
+			<Sheet>
+				<SheetTrigger>Open</SheetTrigger>
+				<SheetContent size="full">
+					<SheetHeader>
+						<SheetTitle>Tour Details</SheetTitle>
+						<SheetDescription
+							><div class="top-24 flex flex-col space-y-2 md:sticky">
+								{#if tour.Location}
+									<div
+										class="flex w-full flex-col justify-center space-y-5 rounded border bg-white p-6 text-center shadow will-change-transform">
+										<ul class="flex flex-col gap-x-2 space-y-2 rounded">
+											<li
+												class="flex flex-row items-center gap-x-2 rounded-sm border p-3 font-semibold">
+												<svg
+													xmlns="http://www.w3.org/2000/svg"
+													class="text-gray-800"
+													width="24"
+													height="24"
+													viewBox="0 0 24 24"
+													><path
+														fill="currentColor"
+														d="M12 4a4 4 0 0 1 4 4a4 4 0 0 1-4 4a4 4 0 0 1-4-4a4 4 0 0 1 4-4m0 10c4.42 0 8 1.79 8 4v2H4v-2c0-2.21 3.58-4 8-4Z" /></svg>
+												For {tour.MinPeople}+ People
+											</li>
+											<li
+												class="flex flex-row items-center gap-2 rounded-sm border p-3 font-semibold">
+												<svg
+													xmlns="http://www.w3.org/2000/svg"
+													class="text-gray-800"
+													width="24"
+													height="24"
+													viewBox="0 0 24 24"
+													><path
+														fill="currentColor"
+														d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10s-4.477 10-10 10zm-3.5-8v2H11v2h2v-2h1a2.5 2.5 0 1 0 0-5h-4a.5.5 0 1 1 0-1h5.5V8H13V6h-2v2h-1a2.5 2.5 0 0 0 0 5h4a.5.5 0 1 1 0 1H8.5z" /></svg>
+												Starting from {tour.Price}$
+											</li>
+											<li
+												class="flex flex-row items-center gap-2 rounded-sm border p-3 font-semibold">
+												<svg
+													xmlns="http://www.w3.org/2000/svg"
+													class="text-gray-800"
+													width="24"
+													height="24"
+													viewBox="0 0 24 24"
+													><path
+														fill="currentColor"
+														d="M12 14q-.425 0-.713-.288T11 13q0-.425.288-.713T12 12q.425 0 .713.288T13 13q0 .425-.288.713T12 14Zm-4 0q-.425 0-.713-.288T7 13q0-.425.288-.713T8 12q.425 0 .713.288T9 13q0 .425-.288.713T8 14Zm8 0q-.425 0-.713-.288T15 13q0-.425.288-.713T16 12q.425 0 .713.288T17 13q0 .425-.288.713T16 14Zm-4 4q-.425 0-.713-.288T11 17q0-.425.288-.713T12 16q.425 0 .713.288T13 17q0 .425-.288.713T12 18Zm-4 0q-.425 0-.713-.288T7 17q0-.425.288-.713T8 16q.425 0 .713.288T9 17q0 .425-.288.713T8 18Zm8 0q-.425 0-.713-.288T15 17q0-.425.288-.713T16 16q.425 0 .713.288T17 17q0 .425-.288.713T16 18ZM5 22q-.825 0-1.413-.588T3 20V6q0-.825.588-1.413T5 4h1V3q0-.425.288-.713T7 2q.425 0 .713.288T8 3v1h8V3q0-.425.288-.713T17 2q.425 0 .713.288T18 3v1h1q.825 0 1.413.588T21 6v14q0 .825-.588 1.413T19 22H5Zm0-2h14V10H5v10Z" /></svg>
+												<!-- For {tours[i].daysCount} Day{#if tours[i].daysCount > 0}s
+										{/if} or More -->
+												Days
+											</li>
+											<Accordion type="single" collapsible class="grid w-full gap-y-2">
+												<AccordionItem value="item-1" class="rounded-sm border px-3">
+													<AccordionTrigger class="py-3"
+														><div class="flex flex-row gap-x-2">
+															<svg
+																xmlns="http://www.w3.org/2000/svg"
+																width="24"
+																height="24"
+																viewBox="0 0 24 24"
+																><path
+																	fill="currentColor"
+																	d="m7 23l3.075-15.55q.15-.725.675-1.088T11.85 6q.575 0 1.063.25T13.7 7l1 1.6q.45.725 1.163 1.313t1.637.862V9H19v14h-1.5V12.85q-1.2-.275-2.225-.875T13.5 10.5l-.6 3l2.1 2V23h-2v-6l-2.1-2l-1.8 8H7Zm.425-9.875l-2.125-.4q-.4-.075-.625-.413t-.15-.762l.75-3.925q.15-.8.85-1.263t1.5-.312l1.15.225l-1.35 6.85ZM13.5 5.5q-.825 0-1.413-.588T11.5 3.5q0-.825.588-1.413T13.5 1.5q.825 0 1.413.588T15.5 3.5q0 .825-.588 1.413T13.5 5.5Z" /></svg
+															>Activities
+														</div>
+													</AccordionTrigger>
+													<AccordionContent>
+														<h4 class="opacity-40">Included, but not limited To</h4>
+														<ul class="grid grid-cols-1 space-y-2 pt-3 font-semibold">
+															{#each list as activity}
+																{#if activityNumbers.includes(activity.id.toString())}
+																	<li class="flex flex-row items-center space-x-2.5 text-base">
+																		<!-- {activity.id} -->
+																		<img
+																			src={`/svgs/${activity.id}.svg `}
+																			alt=""
+																			width="30"
+																			height="30" />
+																		<p>{activity.name}</p>
+																	</li>
+																{/if}
+															{/each}
+														</ul>
+													</AccordionContent>
+												</AccordionItem>
+												<AccordionItem value="item-2" class="rounded-sm border px-3">
+													<AccordionTrigger class="py-3"
+														><div class="flex flex-row gap-x-2">
+															<svg
+																xmlns="http://www.w3.org/2000/svg"
+																width="24"
+																height="24"
+																viewBox="0 0 1024 1024"
+																><path
+																	fill="currentColor"
+																	d="M959 413.4L935.3 372a8 8 0 0 0-10.9-2.9l-50.7 29.6l-78.3-216.2a63.9 63.9 0 0 0-60.9-44.4H301.2c-34.7 0-65.5 22.4-76.2 55.5l-74.6 205.2l-50.8-29.6a8 8 0 0 0-10.9 2.9L65 413.4c-2.2 3.8-.9 8.6 2.9 10.8l60.4 35.2l-14.5 40c-1.2 3.2-1.8 6.6-1.8 10v348.2c0 15.7 11.8 28.4 26.3 28.4h67.6c12.3 0 23-9.3 25.6-22.3l7.7-37.7h545.6l7.7 37.7c2.7 13 13.3 22.3 25.6 22.3h67.6c14.5 0 26.3-12.7 26.3-28.4V509.4c0-3.4-.6-6.8-1.8-10l-14.5-40l60.3-35.2a8 8 0 0 0 3-10.8zM264 621c-22.1 0-40-17.9-40-40s17.9-40 40-40s40 17.9 40 40s-17.9 40-40 40zm388 75c0 4.4-3.6 8-8 8H380c-4.4 0-8-3.6-8-8v-84c0-4.4 3.6-8 8-8h40c4.4 0 8 3.6 8 8v36h168v-36c0-4.4 3.6-8 8-8h40c4.4 0 8 3.6 8 8v84zm108-75c-22.1 0-40-17.9-40-40s17.9-40 40-40s40 17.9 40 40s-17.9 40-40 40zM220 418l72.7-199.9l.5-1.3l.4-1.3c1.1-3.3 4.1-5.5 7.6-5.5h427.6l75.4 208H220z" /></svg
+															>Available Vehicles
+														</div></AccordionTrigger>
+													<AccordionContent>
+														included, but not limited To
+														<ul class="grid grid-cols-1 space-y-2 pt-2 font-semibold">
+															<li class="flex flex-row space-x-2">
+																<p>i</p>
+																<p>Sex</p>
+															</li>
 
-		<div class="relative flex flex-col">
+															<li class="flex flex-row space-x-2">
+																<p>i</p>
+																<p>Sex</p>
+															</li>
+
+															<li class="flex flex-row space-x-2">
+																<p>i</p>
+																<p>Sex</p>
+															</li>
+														</ul>
+													</AccordionContent>
+												</AccordionItem>
+											</Accordion>
+											<ul class="hidden grid-cols-3 border-t">
+												<!-- car svgs  -->
+												<li class="grid place-items-center border-r shadow-inner">
+													<svg
+														class="absolute text-gray-800 transition-all"
+														xmlns="http://www.w3.org/2000/svg"
+														width="24"
+														height="24"
+														viewBox="0 0 24 24"
+														><path
+															fill="currentColor"
+															d="M16 6H6l-5 6v3h2a3 3 0 0 0 3 3a3 3 0 0 0 3-3h6a3 3 0 0 0 3 3a3 3 0 0 0 3-3h2v-3c0-1.11-.89-2-2-2h-2l-3-4M6.5 7.5h4V10h-6l2-2.5m5.5 0h3.5l1.96 2.5H12V7.5m-6 6A1.5 1.5 0 0 1 7.5 15A1.5 1.5 0 0 1 6 16.5A1.5 1.5 0 0 1 4.5 15A1.5 1.5 0 0 1 6 13.5m12 0a1.5 1.5 0 0 1 1.5 1.5a1.5 1.5 0 0 1-1.5 1.5a1.5 1.5 0 0 1-1.5-1.5a1.5 1.5 0 0 1 1.5-1.5Z" /></svg>
+												</li>
+												<li class="grid place-items-center py-2 shadow-inner">
+													<svg
+														class="text-gray-800 transition-all"
+														xmlns="http://www.w3.org/2000/svg"
+														width="24"
+														height="24"
+														viewBox="0 0 24 24"
+														><path
+															fill="currentColor"
+															d="M3 6h13l3 4h2c1.11 0 2 .89 2 2v3h-2a3 3 0 0 1-3 3a3 3 0 0 1-3-3H9a3 3 0 0 1-3 3a3 3 0 0 1-3-3H1V8c0-1.11.89-2 2-2m-.5 1.5V10h8V7.5h-8m9.5 0V10h5.14l-1.89-2.5H12m-6 6A1.5 1.5 0 0 0 4.5 15A1.5 1.5 0 0 0 6 16.5A1.5 1.5 0 0 0 7.5 15A1.5 1.5 0 0 0 6 13.5m12 0a1.5 1.5 0 0 0-1.5 1.5a1.5 1.5 0 0 0 1.5 1.5a1.5 1.5 0 0 0 1.5-1.5a1.5 1.5 0 0 0-1.5-1.5Z" /></svg>
+												</li>
+												<li class="group relative grid place-items-center border-l shadow-inner">
+													<svg
+														class="text-gray-800 transition-all"
+														xmlns="http://www.w3.org/2000/svg"
+														width="24"
+														height="24"
+														viewBox="0 0 512 512"
+														><path
+															fill="currentColor"
+															d="m240.287 127.394l-23.78-19.145l108.861-13.892l2.751 21.828zM180.746 107.7h-62.91l-17.88 44.576h136.067zm275.78 247.033a62.91 62.91 0 1 0-62.911 62.91a62.91 62.91 0 0 0 62.91-62.91zm-279.205 0a62.91 62.91 0 1 0-62.91 62.91a62.91 62.91 0 0 0 62.91-62.91zM486 347.25a20.548 20.548 0 0 1-7.922 16.216a84.917 84.917 0 1 0-168.362 4.388H198.31a84.903 84.903 0 1 0-168.788-13.686a26.834 26.834 0 0 1-3.329-16.504l16.753-143.165a22.914 22.914 0 0 1 22.762-20.26h241.904a14.992 14.992 0 0 1 11.787 5.736l44.012 56.089l105.383 22.941zM337.815 239.255l-35.856-50.656h-93.265v50.683h129.135z" /></svg>
+												</li>
+											</ul>
+										</ul>
+										<div class="grid grid-cols-[3fr_2fr] gap-x-2">
+											<Button class="bg-moss-500 hover:bg-moss-700">Book</Button>
+											<Button variant="secondary">
+												<svg
+													xmlns="http://www.w3.org/2000/svg"
+													width="24"
+													height="24"
+													viewBox="0 0 24 24"
+													class="text-zinc-700"
+													><path
+														fill="currentColor"
+														d="M18 22q-1.25 0-2.125-.875T15 19q0-.175.025-.363t.075-.337l-7.05-4.1q-.425.375-.95.588T6 15q-1.25 0-2.125-.875T3 12q0-1.25.875-2.125T6 9q.575 0 1.1.213t.95.587l7.05-4.1q-.05-.15-.075-.337T15 5q0-1.25.875-2.125T18 2q1.25 0 2.125.875T21 5q0 1.25-.875 2.125T18 8q-.575 0-1.1-.212t-.95-.588L8.9 11.3q.05.15.075.338T9 12q0 .175-.025.363T8.9 12.7l7.05 4.1q.425-.375.95-.587T18 16q1.25 0 2.125.875T21 19q0 1.25-.875 2.125T18 22Zm0-16q.425 0 .713-.288T19 5q0-.425-.288-.713T18 4q-.425 0-.713.288T17 5q0 .425.288.713T18 6ZM6 13q.425 0 .713-.288T7 12q0-.425-.288-.713T6 11q-.425 0-.713.288T5 12q0 .425.288.713T6 13Zm12 7q.425 0 .713-.288T19 19q0-.425-.288-.713T18 18q-.425 0-.713.288T17 19q0 .425.288.713T18 20Zm0-15ZM6 12Zm12 7Z" /></svg
+												></Button>
+										</div>
+									</div>
+									<WeatherCard Location={tour.Location} Lat={tour.Lat} Long={tour.Long} />
+								{:else}
+									<div
+										role="status"
+										class="max-w-full animate-pulse space-y-4 divide-y divide-gray-200 rounded border border-gray-200 p-4 shadow dark:divide-gray-700 dark:border-gray-700 md:p-6">
+										<div class="flex items-center justify-between">
+											<div>
+												<div class="mb-2.5 h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-600" />
+												<div class="h-2 w-32 rounded-full bg-gray-200 dark:bg-gray-700" />
+											</div>
+											<div class="h-2.5 w-12 rounded-full bg-gray-300 dark:bg-gray-700" />
+										</div>
+										<div class="flex items-center justify-between pt-4">
+											<div>
+												<div class="mb-2.5 h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-600" />
+												<div class="h-2 w-32 rounded-full bg-gray-200 dark:bg-gray-700" />
+											</div>
+											<div class="h-2.5 w-12 rounded-full bg-gray-300 dark:bg-gray-700" />
+										</div>
+										<div class="flex items-center justify-between pt-4">
+											<div>
+												<div class="mb-2.5 h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-600" />
+												<div class="h-2 w-32 rounded-full bg-gray-200 dark:bg-gray-700" />
+											</div>
+											<div class="h-2.5 w-12 rounded-full bg-gray-300 dark:bg-gray-700" />
+										</div>
+										<div class="flex items-center justify-between pt-4">
+											<div>
+												<div class="mb-2.5 h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-600" />
+												<div class="h-2 w-32 rounded-full bg-gray-200 dark:bg-gray-700" />
+											</div>
+											<div class="h-2.5 w-12 rounded-full bg-gray-300 dark:bg-gray-700" />
+										</div>
+										<div class="flex items-center justify-between pt-4">
+											<div>
+												<div class="mb-2.5 h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-600" />
+												<div class="h-2 w-32 rounded-full bg-gray-200 dark:bg-gray-700" />
+											</div>
+											<div class="h-2.5 w-12 rounded-full bg-gray-300 dark:bg-gray-700" />
+										</div>
+										<span class="sr-only">Loading...</span>
+									</div>
+								{/if}
+							</div></SheetDescription>
+					</SheetHeader>
+				</SheetContent>
+			</Sheet>
+		</div>
+		<div class="relative hidden flex-col md:flex">
 			<div class="top-24 flex flex-col space-y-2 md:sticky">
 				{#if tour.Location}
 					<div
@@ -266,56 +496,19 @@
 								</li>
 							</ul>
 						</ul>
-						<button
-							type="button"
-							class="bg-moss-500 text-moss-100 w-full rounded-sm py-2.5 text-center text-xl font-semibold opacity-100 transition-all hover:opacity-75"
-							>Book</button>
-					</div>
-					<div class="grid w-full grid-cols-2 gap-2 rounded text-gray-500">
-						<div
-							class="hover:bg-moss-500 group grid place-items-center rounded border bg-white p-2 shadow transition-all hover:text-white">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								width="28"
-								height="28"
-								viewBox="0 0 24 24"
-								class=""
-								><path
-									fill="currentColor"
-									d="M18 22q-1.25 0-2.125-.875T15 19q0-.175.025-.363t.075-.337l-7.05-4.1q-.425.375-.95.588T6 15q-1.25 0-2.125-.875T3 12q0-1.25.875-2.125T6 9q.575 0 1.1.213t.95.587l7.05-4.1q-.05-.15-.075-.337T15 5q0-1.25.875-2.125T18 2q1.25 0 2.125.875T21 5q0 1.25-.875 2.125T18 8q-.575 0-1.1-.212t-.95-.588L8.9 11.3q.05.15.075.338T9 12q0 .175-.025.363T8.9 12.7l7.05 4.1q.425-.375.95-.587T18 16q1.25 0 2.125.875T21 19q0 1.25-.875 2.125T18 22Zm0-16q.425 0 .713-.288T19 5q0-.425-.288-.713T18 4q-.425 0-.713.288T17 5q0 .425.288.713T18 6ZM6 13q.425 0 .713-.288T7 12q0-.425-.288-.713T6 11q-.425 0-.713.288T5 12q0 .425.288.713T6 13Zm12 7q.425 0 .713-.288T19 19q0-.425-.288-.713T18 18q-.425 0-.713.288T17 19q0 .425.288.713T18 20Zm0-15ZM6 12Zm12 7Z" /></svg>
-						</div>
-
-						<div
-							class="hover:bg-moss-500 group flex items-center justify-center gap-1 rounded border bg-white p-2 shadow transition-all hover:text-white">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								class=""
-								width="28"
-								height="28"
-								viewBox="0 0 24 24"
-								><path
-									fill="currentColor"
-									d="m14.777 14.038l2.65-3.92c.262-.386-.235-.805-.615-.529l-2.858 2.015a.571.571 0 0 1-.652 0l-2.116-1.477c-.633-.437-1.538-.277-1.963.335l-2.65 3.92c-.262.386.235.806.615.529l2.858-2.015a.571.571 0 0 1 .652 0l2.116 1.452c.633.462 1.538.302 1.963-.31Z" /><path
-									fill="currentColor"
-									fill-rule="evenodd"
-									d="M12 2.25A9.75 9.75 0 0 0 2.25 12a9.724 9.724 0 0 0 3 7.036V21.5a.75.75 0 0 0 .987.712l2.78-.927A9.745 9.745 0 0 0 12 21.75c5.385 0 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM3.75 12a8.25 8.25 0 1 1 5.516 7.787a.75.75 0 0 0-.486-.004l-2.03.676v-1.75a.75.75 0 0 0-.25-.56A8.228 8.228 0 0 1 3.75 12Z"
-									clip-rule="evenodd" /></svg>
-
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								class=""
-								width="32"
-								height="32"
-								viewBox="0 0 24 24"
-								><path fill="currentColor" d="M16 7a1 1 0 1 1 2 0a1 1 0 0 1-2 0Z" /><path
-									fill="currentColor"
-									fill-rule="evenodd"
-									d="M12 7.25a4.75 4.75 0 1 0 0 9.5a4.75 4.75 0 0 0 0-9.5ZM8.75 12a3.25 3.25 0 1 1 6.5 0a3.25 3.25 0 0 1-6.5 0Z"
-									clip-rule="evenodd" /><path
-									fill="currentColor"
-									fill-rule="evenodd"
-									d="M17.258 2.833a47.721 47.721 0 0 0-10.516 0c-2.012.225-3.637 1.81-3.873 3.832a45.922 45.922 0 0 0 0 10.67c.236 2.022 1.86 3.607 3.873 3.832a47.77 47.77 0 0 0 10.516 0c2.012-.225 3.637-1.81 3.873-3.832a45.914 45.914 0 0 0 0-10.67c-.236-2.022-1.86-3.607-3.873-3.832Zm-10.35 1.49a46.22 46.22 0 0 1 10.184 0c1.33.15 2.395 1.199 2.55 2.517a44.421 44.421 0 0 1 0 10.32a2.89 2.89 0 0 1-2.55 2.516a46.217 46.217 0 0 1-10.184 0a2.89 2.89 0 0 1-2.55-2.516a44.421 44.421 0 0 1 0-10.32a2.89 2.89 0 0 1 2.55-2.516Z"
-									clip-rule="evenodd" /></svg>
+						<div class="grid grid-cols-[3fr_2fr] gap-x-2">
+							<Button class="bg-moss-500 hover:bg-moss-700">Book</Button>
+							<Button variant="secondary">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="24"
+									height="24"
+									viewBox="0 0 24 24"
+									class="text-zinc-700"
+									><path
+										fill="currentColor"
+										d="M18 22q-1.25 0-2.125-.875T15 19q0-.175.025-.363t.075-.337l-7.05-4.1q-.425.375-.95.588T6 15q-1.25 0-2.125-.875T3 12q0-1.25.875-2.125T6 9q.575 0 1.1.213t.95.587l7.05-4.1q-.05-.15-.075-.337T15 5q0-1.25.875-2.125T18 2q1.25 0 2.125.875T21 5q0 1.25-.875 2.125T18 8q-.575 0-1.1-.212t-.95-.588L8.9 11.3q.05.15.075.338T9 12q0 .175-.025.363T8.9 12.7l7.05 4.1q.425-.375.95-.587T18 16q1.25 0 2.125.875T21 19q0 1.25-.875 2.125T18 22Zm0-16q.425 0 .713-.288T19 5q0-.425-.288-.713T18 4q-.425 0-.713.288T17 5q0 .425.288.713T18 6ZM6 13q.425 0 .713-.288T7 12q0-.425-.288-.713T6 11q-.425 0-.713.288T5 12q0 .425.288.713T6 13Zm12 7q.425 0 .713-.288T19 19q0-.425-.288-.713T18 18q-.425 0-.713.288T17 19q0 .425.288.713T18 20Zm0-15ZM6 12Zm12 7Z" /></svg
+								></Button>
 						</div>
 					</div>
 					<WeatherCard Location={tour.Location} Lat={tour.Lat} Long={tour.Long} />
